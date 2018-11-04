11a2ed6ffa4077dce62b56b5f6e6a7a29b10f5b5
Michael Rogers <mprogers@imac-pro.local>
2018-11-04 08:52:19 -0600

    Initial Commit

---
+++ JSON Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JSON Example
+//
+//  Created by Michael Rogers on 11/4/18.
+//  Copyright © 2018 Michael Rogers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
